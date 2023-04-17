50a14b0e65ab94f85a131c327febeeea1f852303
Renan Amorim <renanamorim89@hotmail.com>
2023-04-17 20:14:42 -0300

    alert, confirm e propet

---
+++ aulas/aula - 11 alert, confirm e prompt (Navegador/index.js
@@ -0,0 +1,6 @@
+const num1 = parseFloat(prompt("Digite um numero:"));
+const num2 = parseFloat(prompt("Digite outro numero:"));
+
+const resultado = num1 + num2;
+
+alert(`O resultado foi: ${resultado}`);
